f809e09b18b1ed91d3f5fe322a476bfff2a2614f
shorif <shorifpatwary999@gmail.com>
2024-03-06 12:03:41 +0600

    [plop file] component & story book generator

--- Front-end/plopfile.js
+++ Front-end/plopfile.js
@@ -1,20 +1,45 @@
-export default function (plop) {
-  // story generator
-  plop.setGenerator("story", {
-    description: "application story logic",
+module.exports = function (plop) {
+  // component generator
+  plop.setGenerator("component", {
+    description: "Generate a TypeScript component",
     prompts: [
       {
         type: "input",
         name: "name",
-        message: "story component name please!",
+        message: "Component name:",
       },
     ],
     actions: [
       {
         type: "add",
-        path: "src/stories/{{name}}.stories.ts",
+        path: "src/components/{{dashCase name}}.tsx",
+        templateFile: "plop-templates/component.hbs",
+      },
+    ],
+  });
+
+  // story book  story generator
+  plop.setGenerator("story", {
+    description: "Generate a TypeScript Story",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "Story name:",
+      },
+      {
+        type: "input",
+        name: "sourcePath",
+        message: "Story source path:",
+        default: "components-ui",
+      },
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "src/stories/{{pathCase sourcePath}}/{{snakeCase name}}.stories.ts",
         templateFile: "plop-templates/story.hbs",
       },
     ],
   });
-}
+};
--- Front-end/src/stories/components/ui/Button.stories.ts
+++ Front-end/src/stories/components/ui/Button.stories.ts
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: "Components/UI/Button",
+  title: "components/ui/button",
   component: Button,
   parameters: {
     layout: "centered",
